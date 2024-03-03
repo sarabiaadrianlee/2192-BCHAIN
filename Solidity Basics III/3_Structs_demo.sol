655e0d219c117ac9a4835462bee57ec75d981f3f
sarabiaadrianlee <159108866+sarabiaadrianlee@users.noreply.github.com>
2024-03-03 19:30:09 +0800

    Update 3_Structs_demo.sol

--- Solidity Basics III/3_Structs_demo.sol
+++ Solidity Basics III/3_Structs_demo.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.3;
+pragma solidity ^0.8.20;
 
-contract StructDemo{
-    struct Car{
+    struct Car {
         string model;
         uint year;
         address owner;
     }
+contract CarContract {
 
     Car public car;
     Car[] public cars;
     mapping(address => Car[]) public carsByOwner;
 
-    function someApplications() external{
+    function someApplication() external {
         Car memory toyota = Car("Toyota", 1990, msg.sender);
-        Car memory lambo = Car({year:1980, model:"lamborghini", owner:msg.sender});
+        Car memory lambo = Car({year: 1980, model: "lamborghini", owner: msg.sender});
         Car memory tesla;
 
         tesla.model = "Tesla";
@@ -25,12 +25,13 @@ contract StructDemo{
         cars.push(lambo);
         cars.push(tesla);
 
-        cars.push(Car('Ferari', 2020, msg.sender));
+        cars.push(Car("Ferrari", 2020, msg.sender));
 
         Car storage _car = cars[0];
         _car.year = 1990;
         delete _car.owner;
 
+        // Using delete for dynamic array elements is allowed but not recommended
         delete cars[1];
     }
 }
