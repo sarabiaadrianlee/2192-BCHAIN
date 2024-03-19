6f825c537d5568ae0568d87933ee9d2b56df35ae
sarabiaadrianlee <159108866+sarabiaadrianlee@users.noreply.github.com>
2024-03-19 14:23:08 +0800

    Update Lab_Activity_1.sol

--- Final Lab Activity 1/Lab_Activity_1.sol
+++ Final Lab Activity 1/Lab_Activity_1.sol
@@ -20,3 +20,8 @@ contract EtherWallet {
         return address(this).balance;
     }
 }
+
+//EtherWallet, serves as a digital wallet for managing Ether. It allows the owner to deposit and withdraw Ether securely. 
+//The contract employs a constructor to initialize the owner's address upon deployment and a receive() function to accept incoming Ether transfers. 
+//The withdraw() function enables the owner to withdraw funds while enforcing security measures such as ownership verification and checking for sufficient balance. 
+//Additionally, the getBalance() function allows anyone to check the current balance of the wallet. 
